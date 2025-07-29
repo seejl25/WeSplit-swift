9aec3ea61507a1c8cc783ae4e610ff59b8f9c2cd
seejl25 <seejunlong@u.nus.edu>
2025-07-29 14:06:37 +0800

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by See Jun Long on 29/7/25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
